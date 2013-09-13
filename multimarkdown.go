24b3faaef0992761f94a6f18da9fc39785a6a205
Sam Nguyen <sam.nguyen@sendgrid.com>
2013-09-13 07:35:34 -0700

    Update enums and tests

--- multimarkdown.go
+++ multimarkdown.go
@@ -3,6 +3,7 @@ package multimarkdown
 // #cgo CFLAGS: -Ideps/mmd4
 // #cgo LDFLAGS: -Ldeps/mmd4 -lparse_utilities.o -llatex.o -lparse_utilities.o -lGLibFacade.o -lmemoir.o -lparser.o -lbeamer.o -ltext.o -lcritic.o -lodf.o -lwriter.o -lhtml.o -lopml.o
 // #include <stdlib.h>
+// #include <stdbool.h>
 // #include <libMultimarkdown.h>
 import "C"
 
@@ -10,40 +11,40 @@ import (
     "unsafe"
 )
 
-/*const (*/
-	/*EXT_COMPATIBILITY = C.enum_EXT_COMPATIBILITY [> Markdown compatibility mode <]*/
-	/*EXT_COMPLETE      = C.enum_EXT_COMPLETE      [> Create complete document <]*/
-	/*EXT_HEAD_CLOSED   = C.enum_EXT_HEAD_CLOSED   [> for use by parser <]*/
-	/*EXT_SNIPPET       = C.enum_EXT_SNIPPET       [> Create snippet only <]*/
-	/*EXT_SMART         = C.enum_EXT_SMART         [> Enable Smart quotes <]*/
-	/*EXT_NOTES         = C.enum_EXT_NOTES         [> Enable Footnotes <]*/
-	/*EXT_NO_LABELS     = C.enum_EXT_NO_LABELS     [> Don't add anchors to headers, etc. <]*/
-	/*EXT_FILTER_STYLES = C.enum_EXT_FILTER_STYLES [> Filter out style blocks <]*/
-	/*EXT_FILTER_HTML   = C.enum_EXT_FILTER_HTML   [> Filter out raw HTML <]*/
-	/*EXT_PROCESS_HTML  = C.enum_EXT_PROCESS_HTML  [> Process Markdown inside HTML <]*/
-	/*EXT_NO_METADATA   = C.enum_EXT_NO_METADATA   [> Don't parse Metadata <]*/
-	/*EXT_OBFUSCATE     = C.enum_EXT_OBFUSCATE     [> Mask email addresses <]*/
-	/*EXT_CRITIC        = C.enum_EXT_CRITIC        [> Critic Markup Support <]*/
-	/*EXT_CRITIC_ACCEPT = C.enum_EXT_CRITIC_ACCEPT [> Accept all proposed changes <]*/
-	/*EXT_CRITIC_REJECT = C.enum_EXT_CRITIC_REJECT [> Reject all proposed changes <]*/
-	/*EXT_FAKE          = C.enum_EXT_FAKE          [> 15 is highest number allowed <]*/
-/*)*/
+const (
+    EXT_COMPATIBILITY = 1 << iota // [ > Markdown compatibility mode <]
+    EXT_COMPLETE      = 1 << iota //     [> Create complete document <]
+    EXT_HEAD_CLOSED   = 1 << iota //  [> for use by parser <]
+    EXT_SNIPPET       = 1 << iota //      [> Create snippet only <]
+    EXT_SMART         = 1 << iota //        [> Enable Smart quotes <]
+    EXT_NOTES         = 1 << iota //        [> Enable Footnotes <]
+    EXT_NO_LABELS     = 1 << iota //    [> Don't add anchors to headers, etc. <]
+    EXT_FILTER_STYLES = 1 << iota //[> Filter out style blocks <]
+    EXT_FILTER_HTML   = 1 << iota //  [> Filter out raw HTML <]
+    EXT_PROCESS_HTML  = 1 << iota // [> Process Markdown inside HTML <]
+    EXT_NO_METADATA   = 1 << iota //  [> Don't parse Metadata <]
+    EXT_OBFUSCATE     = 1 << iota //    [> Mask email addresses <]
+    EXT_CRITIC        = 1 << iota //       [> Critic Markup Support <]
+    EXT_CRITIC_ACCEPT = 1 << iota //[> Accept all proposed changes <]
+    EXT_CRITIC_REJECT = 1 << iota //[> Reject all proposed changes <]
+    EXT_FAKE          = 1 << iota //         [> 15 is highest number allowed <]
+)
 
 /* Define output formats we support -- first in list is default */
-/*const (*/
-	/*HTML_FORMAT                  = iota*/
-	/*TEXT_FORMAT                  = iota*/
-	/*LATEX_FORMAT                 = iota*/
-	/*MEMOIR_FORMAT                = iota*/
-	/*BEAMER_FORMAT                = iota*/
-	/*OPML_FORMAT                  = iota*/
-	/*ODF_FORMAT                   = iota*/
-	/*RTF_FORMAT                   = iota*/
-	/*ORIGINAL_FORMAT              = iota [> Not currently used <]*/
-	/*CRITIC_ACCEPT_FORMAT         = iota*/
-	/*CRITIC_REJECT_FORMAT         = iota*/
-	/*CRITIC_HTML_HIGHLIGHT_FORMAT = iota*/
-/*)*/
+const (
+    HTML_FORMAT                  = iota
+    TEXT_FORMAT                  = iota
+    LATEX_FORMAT                 = iota
+    MEMOIR_FORMAT                = iota
+    BEAMER_FORMAT                = iota
+    OPML_FORMAT                  = iota
+    ODF_FORMAT                   = iota
+    RTF_FORMAT                   = iota
+    ORIGINAL_FORMAT              = iota // [> Not currently used <]
+    CRITIC_ACCEPT_FORMAT         = iota
+    CRITIC_REJECT_FORMAT         = iota
+    CRITIC_HTML_HIGHLIGHT_FORMAT = iota
+)
 
 /* These are the identifiers for node types */
 func Version() string {
--- multimarkdown_test.go
+++ multimarkdown_test.go
@@ -1,12 +1,36 @@
 package multimarkdown
 
 import (
-    "testing"
+	"sync"
+	"testing"
 )
 
 func TestVersion(t *testing.T) {
-    v := Version()
-    if v != "4.3" {
-        t.Fatalf("Should return version")
-    }
+	v := Version()
+	if v == "" {
+		t.Fatalf("Should return a version number")
+	}
+}
+
+func TestConvert(t *testing.T) {
+	html := Convert("**hello**", 0, HTML_FORMAT)
+	expected := "<p><strong>hello</strong></p>"
+	if html != expected {
+		t.Errorf("Couldn't convert Markdown!\nIN=%s\nEXPECTED=%s\nACTUAL=%s", "**hello**", expected, html)
+	}
+}
+
+func TestConcurrency(t *testing.T) {
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			TestConvert(t)
+			TestVersion(t)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
 }
