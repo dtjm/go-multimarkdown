258eb71c6d28d0c357f483667709a572d01ee220
Sam Nguyen <sam.nguyen@sendgrid.com>
2013-09-15 07:45:44 -0700

    Add tests

--- multimarkdown.go
+++ multimarkdown.go
@@ -8,45 +8,45 @@ package multimarkdown
 import "C"
 
 import (
-    "unsafe"
+	"fmt"
+	"unsafe"
 )
 
 const (
-    EXT_COMPATIBILITY = 1 << iota // [ > Markdown compatibility mode <]
-    EXT_COMPLETE      = 1 << iota //     [> Create complete document <]
-    EXT_HEAD_CLOSED   = 1 << iota //  [> for use by parser <]
-    EXT_SNIPPET       = 1 << iota //      [> Create snippet only <]
-    EXT_SMART         = 1 << iota //        [> Enable Smart quotes <]
-    EXT_NOTES         = 1 << iota //        [> Enable Footnotes <]
-    EXT_NO_LABELS     = 1 << iota //    [> Don't add anchors to headers, etc. <]
-    EXT_FILTER_STYLES = 1 << iota //[> Filter out style blocks <]
-    EXT_FILTER_HTML   = 1 << iota //  [> Filter out raw HTML <]
-    EXT_PROCESS_HTML  = 1 << iota // [> Process Markdown inside HTML <]
-    EXT_NO_METADATA   = 1 << iota //  [> Don't parse Metadata <]
-    EXT_OBFUSCATE     = 1 << iota //    [> Mask email addresses <]
-    EXT_CRITIC        = 1 << iota //       [> Critic Markup Support <]
-    EXT_CRITIC_ACCEPT = 1 << iota //[> Accept all proposed changes <]
-    EXT_CRITIC_REJECT = 1 << iota //[> Reject all proposed changes <]
-    EXT_FAKE          = 1 << iota //         [> 15 is highest number allowed <]
+	EXT_COMPATIBILITY = 1 << iota // Markdown compatibility mode
+	EXT_COMPLETE                  // Create complete document
+	EXT_HEAD_CLOSED               // for use by parser
+	EXT_SNIPPET                   // Create snippet only
+	EXT_SMART                     // Enable Smart quotes
+	EXT_NOTES                     // Enable Footnotes
+	EXT_NO_LABELS                 // Don't add anchors to headers, etc.
+	EXT_FILTER_STYLES             // Filter out style blocks
+	EXT_FILTER_HTML               // Filter out raw HTML
+	EXT_PROCESS_HTML              // Process Markdown inside HTML
+	EXT_NO_METADATA               // Don't parse Metadata
+	EXT_OBFUSCATE                 // Mask email addresses
+	EXT_CRITIC                    // Critic Markup Support
+	EXT_CRITIC_ACCEPT             // Accept all proposed changes
+	EXT_CRITIC_REJECT             // Reject all proposed changes
+	EXT_FAKE                      // 15 is highest number allowed
 )
 
-/* Define output formats we support -- first in list is default */
+// Define output formats we support -- first in list is default
 const (
-    HTML_FORMAT                  = iota
-    TEXT_FORMAT                  = iota
-    LATEX_FORMAT                 = iota
-    MEMOIR_FORMAT                = iota
-    BEAMER_FORMAT                = iota
-    OPML_FORMAT                  = iota
-    ODF_FORMAT                   = iota
-    RTF_FORMAT                   = iota
-    ORIGINAL_FORMAT              = iota // [> Not currently used <]
-    CRITIC_ACCEPT_FORMAT         = iota
-    CRITIC_REJECT_FORMAT         = iota
-    CRITIC_HTML_HIGHLIGHT_FORMAT = iota
+	HTML_FORMAT = iota // DEFAULT
+	TEXT_FORMAT
+	LATEX_FORMAT
+	MEMOIR_FORMAT
+	BEAMER_FORMAT
+	OPML_FORMAT
+	ODF_FORMAT
+	RTF_FORMAT
+	ORIGINAL_FORMAT // Not currently used
+	CRITIC_ACCEPT_FORMAT
+	CRITIC_REJECT_FORMAT
+	CRITIC_HTML_HIGHLIGHT_FORMAT
 )
 
-/* These are the identifiers for node types */
 func Version() string {
 	var version *C.char = C.mmd_version()
 	defer C.free(unsafe.Pointer(version))
@@ -54,9 +54,11 @@ func Version() string {
 	return C.GoString(version)
 }
 
-func Convert(in string, flags, format int) string {
+func ConvertString(in string, flags, format int) (string, error) {
+	if format > 11 || format == 8 {
+		return "", fmt.Errorf("You must use one of the *_FORMAT constants")
+	}
 	var out *C.char = C.markdown_to_string(C.CString(in), C.int(flags), C.int(format))
 	defer C.free(unsafe.Pointer(out))
-
-    return C.GoString(out)
+	return C.GoString(out), nil
 }
--- multimarkdown_test.go
+++ multimarkdown_test.go
@@ -6,31 +6,49 @@ import (
 )
 
 func TestVersion(t *testing.T) {
+	t.Parallel()
 	v := Version()
 	if v == "" {
 		t.Fatalf("Should return a version number")
 	}
 }
 
-func TestConvert(t *testing.T) {
-	html := Convert("**hello**", 0, HTML_FORMAT)
+func TestConvertString(t *testing.T) {
+	t.Parallel()
+	html, err := ConvertString("**hello**", 0, HTML_FORMAT)
 	expected := "<p><strong>hello</strong></p>"
-	if html != expected {
+	if err != nil || html != expected {
 		t.Errorf("Couldn't convert Markdown!\nIN=%s\nEXPECTED=%s\nACTUAL=%s", "**hello**", expected, html)
 	}
 }
 
 func TestConcurrency(t *testing.T) {
+	t.Parallel()
 	wg := sync.WaitGroup{}
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 16; i++ {
 		wg.Add(1)
 		go func() {
-			TestConvert(t)
-			TestVersion(t)
+			ConvertString("**hello**", 0, HTML_FORMAT)
+			Version()
 			wg.Done()
 		}()
 	}
 
 	wg.Wait()
+}
+
+func TestBadFormat(t *testing.T) {
+	t.Parallel()
+
+	_, err := ConvertString("**hello**", 0, 12)
+	if err == nil {
+		t.Errorf("Should return an error for invalid format")
+	}
+}
+
+func BenchmarkConvertString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ConvertString("*hello*", 0, HTML_FORMAT)
+	}
 }
